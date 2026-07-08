819c138168174f1b170ea289825ef2dc04bc6768
zahinmahmud <iamdiptoboy@gmail.com>
2026-07-08 17:44:06 +0600

    Updated file

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <h2>Hello from </h2>
+    </div>
+  )
+
+}
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div> <h2>hello from navbar </h2></div>
+  )
+}
+
+export default Navbar
