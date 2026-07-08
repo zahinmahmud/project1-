251004678791f71ae703bd5c645d570cc764e9c3
zahinmahmud <iamdiptoboy@gmail.com>
2026-07-08 18:33:13 +0600

    add props to card

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,13 @@ function App() {
     <div>
       <Navbar/>
       <div style={{display:'flex'}}>
-        <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
+        {/* <Card/> */}
+      <Card title="Allion" description ="Allion is  toyota brand Car"/>
+      <Card title="Axio" description ="Axio is  toyota brand Car"/>
+      <Card title="Premio" description ="Premio is  toyota brand Car"/>
+      <Card title="Prius" description ="Prius is  toyota brand Car"/>
+      <Card title="Rav4" description ="Rav4 is  toyota brand Car"/>
+
       </div>
       <h2>Hello from </h2>
     </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import "./Card.css"
 
-function Card() {
-    const  title = "Card Title";
-    const  description  = "A card is a versatile, flat, rectangular piece of stiff paper, cardboard, or plastic used to store information, convey messages, or play games. Common physical forms include paper like playing cards, postcards, and greeting cards, or durable plastics for identification and financial transactions."
+function Card(props) {
     const date = new Date()
     const month = date.getMonth()
     const currentYear = date.getFullYear();
   return (
     <div className='container'>
-        <h2>{title}</h2>
-        <p>{description}</p>
+        <h2>{props.title}</h2>
+        <p>{props.description}</p>
         <p>Created on {date.toDateString()}:{month}:{currentYear}</p>
     </div>
   )
